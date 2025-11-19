b21fefdc26fea1c4c1830014a5c48d75fda872f8
LI Jiaxiu <royli2004@outlook.com>
2025-11-20 01:38:27 +0800

    Update data preprocessing

--- data/read_proc_data.py
+++ data/read_proc_data.py
@@ -6,14 +6,20 @@ from random import random
 def get_data_summary(root: str) -> Tuple[int, int]:
     path = os.path.join(root, "summary.txt")
     with open(path, "r") as f:
-        f.readline()
-        f.readline()
         num_users = int(f.readline().strip().split(":")[1])
         num_items = int(f.readline().strip().split(":")[1])
     return num_users, num_items
 
 
-def load_proc_data(root: str) -> Tuple[List[Tuple[int, int]], Dict[int, Set[int]], Dict[int, Set[int]]]:
+def load_proc_data(
+    root: str,
+) -> Tuple[
+    List[Tuple[int, int]],
+    List[Tuple[int, int]],
+    Dict[int, Set[int]],
+    Dict[int, Set[int]],
+]:
+    interactions: List[Tuple[int, int]] = []
     train: List[Tuple[int, int]] = []
     val: Dict[int, Set[int]] = {}
     path = os.path.join(root, "train.tsv")
@@ -21,14 +27,13 @@ def load_proc_data(root: str) -> Tuple[List[Tuple[int, int]], Dict[int, Set[int]
         f.readline()
         for line in f:
             user_procid, item_procid = line.strip().split("\t")
-            user_procid = int(user_procid)
-            item_procid = int(item_procid)
-            if random() > 0.9:
+            user_procid, item_procid = int(user_procid), int(item_procid)
+            interactions.append((user_procid, item_procid))
+            if random() > 0.1:
                 train.append((user_procid, item_procid))
-            elif user_procid in val:
-                val[user_procid].add(item_procid)
-            else:
-                val[user_procid] = {item_procid}
+            elif user_procid not in val:
+                val[user_procid] = {}
+            val[user_procid].add(item_procid)
 
     test: Dict[int, Set[int]] = {}
     path = os.path.join(root, "test.tsv")
@@ -36,11 +41,10 @@ def load_proc_data(root: str) -> Tuple[List[Tuple[int, int]], Dict[int, Set[int]
         f.readline()
         for line in f:
             user_procid, item_procid = line.strip().split("\t")
-            user_procid = int(user_procid)
-            item_procid = int(item_procid)
-            if user_procid in test:
-                test[user_procid].add(item_procid)
-            else:
-                test[user_procid] = {item_procid}
+            user_procid, item_procid = int(user_procid), int(item_procid)
+            interactions.append((user_procid, item_procid))
+            if user_procid not in test:
+                test[user_procid] = {}
+            test[user_procid].add(item_procid)
 
-    return train, val, test
+    return interactions, train, val, test
